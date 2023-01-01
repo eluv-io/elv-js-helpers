8bc1f20b33ac53dc15595cbbda256a6a3279d2ba
eponymous301 <michael.parker@eluv.io>
2023-01-01 14:18:15 -0800

    * Change unit test description for JSDoc examples to 'should have a working example in JSDoc'  * Change unit test generator for JSDoc examples to use test helper, change `function()` to `() =>`, insert JSDoc unit test as first test instead of last test (to avoid interactions with any vars declared in `describe` block)

--- bin/processJSDocExample.js
+++ bin/processJSDocExample.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 const path = require('path')
 
 const JSDOC_EXAMPLE_RE = /^ \* @example((.|\n)+^) *\*\//m
-const JSDOC_TEST_NAME = 'should have a correct example in JSDoc'
-const PRETTIER_BIN_PATH = path.resolve(__dirname, '..', 'node_modules','prettier','bin-prettier.js')
+const JSDOC_TEST_NAME = 'should have a working example in JSDoc'
+const PRETTIER_BIN_PATH = path.resolve(__dirname, '..', 'node_modules', 'prettier', 'bin-prettier.js')
 
-const testRequireText = srcFileAbsPath => `TH.requireSrcFile('${srcFileAbsPath.split('/src/')[1].slice(0,-3)}')`
-const jsDocRequireMatch = srcFileAbsPath => `require('@eluvio/elv-js-helpers/${srcFileAbsPath.split('/src/')[1].slice(0,-3)}')`
+const testRequireText = srcFileAbsPath => `TH.requireSrcFile('${srcFileAbsPath.split('/src/')[1].slice(0, -3)}')`
+const jsDocRequireMatch = srcFileAbsPath => `require('@eluvio/elv-js-helpers/${srcFileAbsPath.split('/src/')[1].slice(0, -3)}')`
 
 const jsDocExample = filePath => {
   const scriptCode = fs.readFileSync(filePath).toString()
@@ -25,7 +25,7 @@ const jsDocTest = (jsDocExampleText, srcFileAbsPath) => {
 
   // handle example lines that output to console
   const consoleSearch = /(.+)( +\/\/=> OUTPUT: +)(.+)/
-  const consoleReplacer = (_, code, divider, outputString) => `\nthis.sinon.stub(console,'log')\n${code}\nexpect(console.log.calledWith(${outputString})).to.be.true\nthis.sinon.restore()\n`
+  const consoleReplacer = (_, code, divider, outputString) => `\nTH.sinon.stub(console,'log')\n${code}\nTH.expect(console.log.calledWith(${outputString})).to.be.true\nTH.sinon.restore()\n`
 
   // handle example lines that return values
   const shouldSearch = /(.+)( +\/\/=> +)(.+)/
@@ -36,14 +36,19 @@ const jsDocTest = (jsDocExampleText, srcFileAbsPath) => {
     .split('\n')
     .filter(x => !x.includes(requireFindText)) // remove require statement for the src file
     .map(x => x.replace(/^ *\*/, '')) // first * of each line
-    .map(x => x.replace('@eluvio/elv-js-helpers/', '../../../../src/')) // change 'includes'
+    .map(
+      x => x.replace(
+        'require(\'@eluvio/elv-js-helpers/',
+        'TH.requireSrcFile(\''
+      )
+    ) // change 'includes'
     .map(x => x.replace(exceptionSearch, exceptionReplacer)) // process assertions related to exceptions
     .map(x => x.replace(consoleSearch, consoleReplacer)) // process assertions related to console output
     .map(x => x.replace(shouldSearch, shouldReplacer)) // process assertions that return values
     .filter(x => !/^\w*$/.test(x)) // remove blank lines
     .join('\n')
 
-  return `it('${JSDOC_TEST_NAME}', function() {\n${jsDocAssertions}\n})`
+  return `it('${JSDOC_TEST_NAME}', () => {\n${jsDocAssertions}\n})`
 }
 
 const newUnitTestFileText = (srcFileAbsPath, jsDocAssertions) => {
@@ -56,7 +61,7 @@ ${requireStmt}
 describe('${srcFileBasename}', () => {
   ${jsDocAssertions}
 
-  // it('should... ', function() {
+  // it('should... ', () => {
   //
   // })
 })
@@ -66,7 +71,7 @@ describe('${srcFileBasename}', () => {
 
 const prettify = filePath => child_process.spawnSync(PRETTIER_BIN_PATH, ['-w', filePath])
 
-module.exports  = {
+module.exports = {
   jsDocExample,
   jsDocTest,
   JSDOC_EXAMPLE_RE,
