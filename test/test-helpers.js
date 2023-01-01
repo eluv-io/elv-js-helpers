74aed160676e48e06d1867922c0b0ed58743c1b5
eponymous301 <michael.parker@eluv.io>
2023-01-01 13:39:03 -0800

    Remove mocha-sinon from test helper and package.json, add sinon to test helper

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -1,14 +1,17 @@
+// helper to reduce boilerplate in tests
 const path = require('path')
 
 const chai = require('chai')
+const sinon = require('sinon')
+
 chai.should()
 const expect = chai.expect
-require('mocha-sinon')
 
 const requireSrcFile = subDirAndFilename => require(path.join(__dirname, '../src/', subDirAndFilename))
 
 module.exports = {
   chai,
   expect,
-  requireSrcFile
+  requireSrcFile,
+  sinon
 }
