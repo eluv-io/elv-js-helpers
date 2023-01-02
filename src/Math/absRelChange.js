b09f0b42303abd9233f6a489f3ed354a32f6b9e4
eponymous301 <michael.parker@eluv.io>
2023-01-01 22:53:33 -0800

    * rebuild docs and main.js  * update README - move API docs link to top, add section on code naming conventions  * update absRelChange to use growth.js

--- src/Math/absRelChange.js
+++ src/Math/absRelChange.js
@@ -1,4 +1,4 @@
-const relChange = require('./relChange')
+const growth = require('./growth')
 
 const curry = require('../Functional/curry')
 
@@ -7,7 +7,7 @@ const curry = require('../Functional/curry')
 // e.g. returns 0 if the values are the same, 0.5 if the second value is 50% larger
 // Note that 0 is invalid for oldValue, the relative change is Infinity in that case
 const absRelChange = curry(
-  (oldValue, newValue) => Math.abs(relChange(oldValue, newValue))
+  (oldValue, newValue) => Math.abs(growth(oldValue, newValue))
 )
 
 module.exports = absRelChange
