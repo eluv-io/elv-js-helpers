40262b317f57f3e534708433a1f88ad16e7d1e65
eponymous301 <michael.parker@eluv.io>
2024-06-24 09:32:58 -0700

    * Update docs

---
+++ test/unit/jsdoc_examples/ADT/Async.jsdoc.test.js
@@ -0,0 +1,7 @@
+// AUTO-GENERATED TEST - DO NOT MODIFY, CHANGES WILL BE OVERWRITTEN DURING BUILD
+const TH = require('../../../test-helpers')
+const Async = TH.requireSrcFile('ADT/Async')
+
+describe('Async JSDoc example', () => {
+  it('should execute correctly as described', () => {})
+})
